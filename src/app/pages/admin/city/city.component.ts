a73b6e510335b3af67f3a3252ea42328b192b06e
Simon Bengtsson <sillen.00@hotmail.com>
2024-07-28 21:34:24 +0200

    Trying to show cities from a free api.

--- src/app/pages/admin/city/city.component.ts
+++ src/app/pages/admin/city/city.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-city',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './city.component.html',
   styleUrl: './city.component.scss'
 })
-export class CityComponent {
+
+export class CityComponent implements OnInit {
+
+  cityList: any [] = [];
+  constructor( private http: HttpClient ) {}
+
+  ngOnInit(): void {
+    this.getAllCity()
+  }
+
+  getAllCity() {
+    this.http.get("https://freeapi.gerasim.in/api/FlightBooking/GetAllCity").subscribe((res:any) => {
+      this.cityList = res.data;
+    })
+  }
 
 }
